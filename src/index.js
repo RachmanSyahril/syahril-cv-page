b9080db5bc7cabdee0bf7d9910392d0c72ca0717
Syahril Rachman <rachman.syahril@gmail.com>
2024-07-02 15:09:42 +0700

    added measuring id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import CVPage from "./CVPage";
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("YOUR_MEASURING_ID");
+ReactGA.initialize("G-Y98L0ZT1M1");
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
