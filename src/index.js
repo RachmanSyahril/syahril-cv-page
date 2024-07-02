47e893188aa5ddd87e5bf736a544d2796f3f7ed4
Syahril Rachman <rachman.syahril@gmail.com>
2024-07-02 15:06:53 +0700

    added react-ga4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import CVPage from "./CVPage";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("YOUR_MEASURING_ID");
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
